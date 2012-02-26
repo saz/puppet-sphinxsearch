9f0737b4609582e02d636c892a11cc745bb13156
Steffen Zieger <me@saz.sh>
2012-02-26 18:18:39 +0100

    first check, then commit... add the missing letter "s" to "service"

--- manifests/init.pp
+++ manifests/init.pp
@@ -86,7 +86,7 @@ class sphinxsearch(
     purge   => true,
     force   => true,
     require => Package[$package],
-    notify  => Service[$ervice],
+    notify  => Service[$service],
   }
 
   file { $work_dir:
