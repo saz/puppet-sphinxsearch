0810ce3cd5fb1578868ff766a6d00cbe9e0b7c8a
Steffen Zieger <me@saz.sh>
2012-02-26 23:26:21 +0100

    add docheader

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,98 @@
+# Class: sphinxsearch
+#
+# This module manages sphinxsearch
+#
+# Parameters:
+#   [*ensure*]
+#     Ensure if present or absent.
+#     Default: present
+#
+#   [*instances*]
+#     List of instances to use
+#     Default: [0]
+#
+#   [*config_source*]
+#     Directory to fetch config files from.
+#     NOTE: Config files must be named like 'sphinx_<instance_id>.conf' or
+#           'sphinx.conf' if there is only instance 0.
+#     Default: undef
+#
+#   [*config_dir*]
+#     Directory where config files resist.
+#     Only set this, if your platform is not supported or you know, what you're doing.
+#     Default: auto-set, platform specific
+#
+#   [*default_file*]
+#     Full path and name of default file.
+#     Only set this, if your platform is not supported or you know, what you're doing.
+#     Default: auto-set, platform specific
+#
+#   [*work_dir*]
+#     Name of the directory for index files.
+#     Only set this, if your platform is not supported or you know, what you're doing.
+#     Default: auto-set, platform specific
+#
+#   [*work_dir_recurse*]
+#     Manage work_dir recursive (change owner/group, modes)
+#     Default: false
+#
+#   [*work_dir_mode*]
+#     Mode for files and directories in work_dir.
+#     Default: 0640
+#
+#   [*user*]
+#     Name of the user running searchd.
+#     Only set this, if your platform is not supported or you know, what you're doing.
+#     Default: auto-set, platform specific
+#
+#   [*group*]
+#     Name of the group running searchd.
+#     Only set this, if your platform is not supported or you know, what you're doing.
+#     Default: auto-set, platform specific
+#
+#   [*autoupgrade*]
+#     Upgrade package automatically, if there is a newer version.
+#     Default: false
+#
+#   [*package*]
+#     Name of the package.
+#     Only set this, if your platform is not supported or you know, what you're doing.
+#     Default: auto-set, platform specific
+#
+#   [*service*]
+#     Name of NTP service
+#     Only set this, if your platform is not supported or you know, what you're doing.
+#     Default: auto-set, platform specific
+#
+#   [*service_ensure*]
+#     Ensure if service is running or stopped
+#     Default: running
+#
+#   [*service_enable*]
+#     Start service at boot
+#     Default: true
+#
+#   [*service_hasstatus*]
+#     Service has status command
+#     Only set this, if your platform is not supported or you know, what you're doing.
+#     Default: auto-set, platform specific
+#
+#   [*service_hasrestart*]
+#     Service has restart command
+#     Only set this, if your platform is not supported or you know, what you're doing.
+#     Default: auto-set, platform specific
+#
+# Actions:
+#   Installs sphinxsearch and configures one or more instances.
+#
+# Requires:
+#   puppetlabs/stdlib
+#
+# Sample Usage:
+#   class { 'sphinxsearch': }
+#
+#
+# [Remember: No empty lines between comments and class definition]
 class sphinxsearch(
   $ensure = 'present',
   $instances = [0],
@@ -10,6 +105,8 @@ class sphinxsearch(
   $user = $sphinxsearch::params::user,
   $group = $sphinxsearch::params::group,
   $autoupgrade = false,
+  $package = $sphinxsearch::params::package
+  $service = $sphinxsearch::params::service,
   $service_ensure = 'running',
   $service_enable = true,
   $service_hasstatus = $sphinxsearch::params::service_hasstatus,
@@ -20,8 +117,6 @@ class sphinxsearch(
   $firewall_tool = false,
   $firewall_src = false,
   $firewall_dst = false,
-  $service = $sphinxsearch::params::service,
-  $package = $sphinxsearch::params::package
 ) inherits sphinxsearch::params {
 
   validate_bool(
